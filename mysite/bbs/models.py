d408ef68168bfa2992e7cd0ffdbaf2d4db0d183f
arsdeeennya <kkamazonkk@yahoo.co.jp>
2021-04-25 15:54:49 +0900

    create bbs model

--- mysite/bbs/models.py
+++ mysite/bbs/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Bbs(models.Model):
+    name = models.CharField(max_length=20, default='森のくまさん')
+    message = models.TextField()
+    ip = models.CharField(max_length=50)
+    week = models.CharField(max_length=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('bbs/', include('bbs.urls')),
 ]
-urlpatters += static(settings.MEDIA_URL, document_root=settings.MEDIA_URL)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_URL)
 
