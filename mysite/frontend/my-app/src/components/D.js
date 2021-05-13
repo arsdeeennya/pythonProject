37afd84accad113352100e368d3191754d3fe6e1
arsdeeennya <kkamazonkk@yahoo.co.jp>
2021-05-14 08:27:27 +0900

    機能追加

--- mysite/frontend/my-app/src/components/D.js
+++ mysite/frontend/my-app/src/components/D.js
@@ -2,19 +2,32 @@ import React,{useState} from 'react';
 import '../d.css';
 
 export const D = () => {
+  const [todoText, setTodoText] = useState('')
   const [imcomplate, setimcomplate] = useState(['aaa'],['iiii'])
+  const [complate, setcomplate] = useState(['uuuu'])
+
+  const onChangeTodoText = (event) => setTodoText(event.target.value)
+  const onClickTodoText = () => {
+    if(todoText !== ''){
+      const newTodos = [...imcomplate, todoText]
+      setimcomplate(newTodos)
+      setTodoText('')
+    }
+  }
+
+
   return (
     <>
       <div className='input-area'>
-        <input placeholder='TODOを入力'/>
-        <button>追加</button>
+        <input placeholder='入力してちょ' value={todoText} onChange={onChangeTodoText}/>
+        <button onClick={onClickTodoText}>追加</button>
       </div>
       <div className='incomplete-area'>
         <p className='title'>未完了のTODO</p>
         <ul>
           {imcomplate.map((todo) => {
             return(
-              <div className='list-row'>
+              <div key='todo' className='list-row'>
                 <li>{todo}</li>
                 <button>完了</button>
                 <button>削除</button>
@@ -26,10 +39,14 @@ export const D = () => {
       <div className='complete-area'>
         <p className='title'>完了のTODO</p>
         <ul>
-          <div>
-            <li>ああああ</li>
-            <button>戻す</button>
-          </div>
+          {complate.map((todo) => {
+            return(
+              <div key='todo' className='list-row'>
+                <li>{todo}</li>
+                <button>戻す</button>
+              </div>
+            )
+          })}
         </ul>
       </div>
     </>
