6727ce05a4ef7e23a42c040c946af54967871603
arsdeeennya <kkamazonkk@yahoo.co.jp>
2021-05-29 08:18:52 +0900

    postできるように

--- mysite/tspro/my-app/src/components/ThreadForm.tsx
+++ mysite/tspro/my-app/src/components/ThreadForm.tsx
@@ -3,6 +3,8 @@ import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import CreateIcon from '@material-ui/icons/Create';
 import styled from 'styled-components'
+import axios from "axios";
+
 
 const Responce = styled.div`
   margin: 90px 110px 0px;
@@ -48,21 +50,45 @@ const useStyles = makeStyles((theme: Theme) =>
 );
 
 const ThreadForm: React.FC =  () => {
-  const [texts, setTexts] = useState('');
+  const [name, setName] = useState('');
+  const [comment, setComment] = useState('');
   const classes = useStyles();
 
-  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setTexts(e.target.value)
+  const nameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value)
   }
-  console.log(texts)
+
+  const commentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setComment(e.target.value)
+  }
+
+  const submit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const post = {
+      name: name,
+      message: comment
+    }
+
+    axios.post('http://127.0.0.1:8000/bbs/index/', post,{
+      headers: { "Content-Type": "application/json" },
+    })
+    .then(res => {
+      console.log(res);
+      console.log(res.data);
+    })
+    .catch(res => {
+      console.log(post)
+    })
+  }
+
   return (
     <React.Fragment>
       <Responce>
         <Post>レスを投稿する</Post>
-        <form>
-          <Name placeholder={'名前(省略可)'} size={70}/>
-          <Comment placeholder={'コメント内容'} rows={5} cols={70} value={texts} onChange={handleChange}/>
-          <Write variant="contained" color="primary" className={classes.button} endIcon={<CreateIcon/>}>書き込む</Write>
+        <form onSubmit={submit} >
+          <Name placeholder={'名前(省略可)'} size={70} name="name" value={name} onChange={nameChange}/>
+          <Comment placeholder={'コメント内容'} rows={5} cols={70} name="message" value={comment} onChange={commentChange}/>
+          <Write variant="contained" color="primary" className={classes.button} endIcon={<CreateIcon/>} type='submit'>書き込む</Write>
         </form>
       </Responce>
     </React.Fragment>
