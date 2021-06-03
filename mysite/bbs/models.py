662e282772db7393c654260d34613a028ccd0483
arsdeeennya <kkamazonkk@yahoo.co.jp>
2021-06-04 00:17:09 +0900

    ipげっと

--- mysite/bbs/models.py
+++ mysite/bbs/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+import socket
+import base64
 
 class Bbs(models.Model):
     name = models.CharField(max_length=20, default='森のくまさん')
     message = models.TextField()
-    ip = models.CharField(max_length=50)
+    ip = socket.gethostbyname(socket.gethostname())
+    encoded_ip = base64.b64encode(ip.encode('ascii')).decode('ascii')[:-2]
+    ip = models.CharField(max_length=50, default=encoded_ip)
     week = models.CharField(max_length=2)
     created_at = models.DateTimeField(auto_now_add=True)
 
