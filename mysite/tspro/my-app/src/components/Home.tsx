3bab9997dc8e7f29c05952baa612364b36bdcc51
arsdeeennya <kkamazonkk@yahoo.co.jp>
2021-06-04 13:27:49 +0900

    headerちょうせい

--- mysite/tspro/my-app/src/components/Home.tsx
+++ mysite/tspro/my-app/src/components/Home.tsx
@@ -2,21 +2,27 @@ import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import styled from 'styled-components';
+
+const Main = styled.main`
+  background-color: #f2f3f7;
+`
 
 const Home: React.FC =  () => {
 
   const useStyles = makeStyles((theme) => ({
     heroContent: {
-      backgroundColor: theme.palette.background.paper,
       padding: theme.spacing(8, 0, 6),
     },
   }));
 
   const classes = useStyles();
 
+  
+
   return (
     <React.Fragment>
-      <main>
+      <Main>
         <div className={classes.heroContent}>
           <Container maxWidth="sm">
             <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
@@ -27,7 +33,7 @@ const Home: React.FC =  () => {
             </Typography>
           </Container>
         </div>
-      </main>
+      </Main>
     </React.Fragment>
   );
 }
