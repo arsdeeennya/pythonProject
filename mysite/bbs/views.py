17a396de1349dd36e1801b83b0b14ebeb9a5c7e0
arsdeeennya <kkamazonkk@yahoo.co.jp>
2021-06-10 13:49:59 +0900

    2行開けないとエラーになるから

--- mysite/bbs/views.py
+++ mysite/bbs/views.py
@@ -2,6 +2,7 @@ from .models import Bbs
 from .serializers import BbsSerializer
 from rest_framework import generics
 
+
 class BbsListCreate(generics.ListCreateAPIView):
     queryset = Bbs.objects.all()
     serializer_class = BbsSerializer
